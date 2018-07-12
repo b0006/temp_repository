f11f8f8b6a711aedd0b30ecfa4356ce915590ff4
Alexandr Kasikov <qsefthuken@gmail.com>
2018-07-12 16:04:51 +0700

    update

--- js/script.js
+++ js/script.js
@@ -1,10 +1,30 @@
 $(document).ready(function(){
 
+    // let player = "white";
+
     let but = null;
+
+    function checkPlayer(object) {
+        if(object.classList.contains("black")) {
+            return "black";
+        }
+        else if(object.classList.contains("white"))
+        {
+            return "white";
+        }
+    }
 
     // click on checkers
     addDynamicEventListener(document.body, 'click', '.piece', function (e) {
         but = e.target;
+
+        //check player
+        // let currentPlayer = checkPlayer(e.target);
+        //
+        // if(player !== currentPlayer) {
+        //     alert("Сейчас ходит " + player);
+        //     return false;
+        // }
 
         //remove active from all pieces
         $(".piece").removeClass("active");
@@ -52,21 +72,10 @@ $(document).ready(function(){
                         }
                     }
 
-                    console.log(potencial_x + ", " + potencial_y);
+                    // console.log(potencial_x + ", " + potencial_y);
                 }
             }
         }
-
-        if(e.target.classList.contains('black')){
-
-
-        }
-        else {
-            allPiece = document.querySelectorAll('.piece.white');
-        }
-
-
-
 
     });
 
@@ -75,15 +84,9 @@ $(document).ready(function(){
 
         $('.rank__check').removeClass("over");
 
-        console.log(event.target.getAttribute("x") + ', ' + event.target.getAttribute("y"));
-
         try {
             if (event.target.firstChild.classList.contains('active')) {
-                // let div = document.createElement("div");
-                // div.className = "piece black";
-                // div.textContent = 'T';
-                //
-                // this.appendChild(div);
+
             }
         }catch (e) {
             if(but){
@@ -101,44 +104,26 @@ $(document).ready(function(){
                     currentColor = "white";
                 }
 
+                $(but).removeClass("active");
 
                 if(currentColor === "black") {
                     if (((but_y - current_y) === -1) && ((but_x - current_x === 1) || (but_x - current_x === -1))) {
-
-                        $(but).removeClass("active");
-
                         // createElement
-                        let div = document.createElement("div");
-                        div.className = "piece black";
-                        div.innerHTML = '&#9820;';
-
-                        event.target.appendChild(div);
-
-                        // removeElement
-                        but.remove();
-
-                        but = null;
+                        $(event.target).append('<div class="piece black">&#9820;</div>');
                     }
                 }
                 else {
                     if (((but_y - current_y) === 1) && ((but_x - current_x === 1) || (but_x - current_x === -1))) {
-                        $(but).removeClass("active");
-
                         // createElement
-
                         $(event.target).append('<div class="piece white">&#9814;</div>');
-                        // let div = document.createElement("div");
-                        // div.className = "piece white";
-                        // div.innerHTML = '&#9814;';
-                        //
-                        // this.append(div);
 
-                        // removeElement
-                        but.remove();
-
-                        but = null;
                     }
                 }
+
+                // removeElement
+                $(but).remove();
+                but = null;
+
             }
         }
     });
